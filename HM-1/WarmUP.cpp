f97ba37100a8723abce48a3eacd5faf616eebd8d
alona <alyona2012951@gmail.com>
2019-11-26 02:55:00 +0200

    First homeWorks

---
+++ HM-1/WarmUP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {int a = 4+5, b = 31-6, c = 1, d=d*2; 
+ //есть ли разница в данном случае между сторочкой кода 1 и 2
+ //1) int d=d*2; cout << "d:"<< d << endl; 
+ //2) int d; cout << "d:"<< d*2 << endl;
+   cout << "a:" << a << endl;
+   cout << "b:"<< b << endl;
+   cout << "c:"<< c << endl;
+   cout << "d:"<< d << endl;
+    // не знаю почему и каким образом оно опредляет численное значение переменнной "d", но это работает 
+ } 
